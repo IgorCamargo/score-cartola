bb393409fabc7860de771acf272b51519155002a
Igor Camargo <igorfelcam@hotmail.com>
2019-05-01 00:06:10 -0300

    feat:component to create group

---
+++ src/views/MakeGroup.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Criar Grupo {{ returnStorage }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MakeGroup',
+  data(){
+    return {
+      returnStorage: ''
+    }
+  },
+  mounted: function() {
+    console.log('here');
+    this.returnStorage = localStorage.getItem('newGroup');
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
